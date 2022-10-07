ed22f57c1fea17d5bcb50d90cdf74d77e947a7ac
giusscos <giu.cosenza08@gmail.com>
2022-10-07 18:00:31 +0200

    Add banner header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,13 @@
 <header>
+    <div class="banner_header">
+        <span class="uppercase"> 
+            dc power visa&reg;
+        </span>
+        <select class="uppercase">
+            <option disabled selected>additional dc sites</option>
+            <option>bho</option>
+        </select>
+    </div>
     <div class="container">
         <div class="logo_wrapper">
             <img src="./img/dc-logo.png" alt="logo-dc" />
